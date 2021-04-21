8dc8b96cb624710ab8b938a862cdae983479b3fa
Rodolfo Roman <rromanve@southtexascollege.edu>
2021-04-21 15:19:46 -0500

    Login Button Layout

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,6 +1,6 @@
 <template>
   <nav class="bg-coolGray-900">
-    <div class="container mx-auto">
+    <div class="container mx-auto flex justify-between items-center">
       <div class="flex items-center space-x-4">
         <noto:videocassette class="w-16 h-16" />
         <p
@@ -9,10 +9,15 @@
           Movies<span class="font-bold">DB</span>
         </p>
       </div>
+      <div>
+        <button class="bg-green-600 text-coolGray-100 font-semibold hover:bg-green-800 px-8 py-2 rounded-full">
+          Login
+        </button>
+      </div>
     </div>
   </nav>
 </template>
 
 <script setup>
-  import tmdbLogo from '~/assets/tmdb.svg'
+import tmdbLogo from '~/assets/tmdb.svg'
 </script>
